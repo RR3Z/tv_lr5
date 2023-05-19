f38b46f76f7a7fbc0b595f8b27b62775bb6b68e0
MatiasMoore <nikita.birukov.03@mail.ru>
2023-05-19 21:55:19 +0300

    Убрал мусор из main

--- launch.py
+++ launch.py
@@ -403,33 +403,6 @@ class MainWindow(QMainWindow):
  
         
 if __name__ == "__main__":
-    
-    if False:
-        from mpl_toolkits.axisartist.axislines import SubplotZero
-        import matplotlib.pyplot as plt
-        import numpy as np
-
-
-        fig = plt.figure()
-        ax = SubplotZero(fig, 111)
-        fig.add_subplot(ax)
-
-        for direction in ["xzero", "yzero"]:
-            # adds arrows at the ends of each axis
-            ax.axis[direction].set_axisline_style("-|>")
-
-            # adds X and Y-axis from the origin
-            ax.axis[direction].set_visible(True)
-
-        for direction in ["left", "right", "bottom", "top"]:
-            # hides borders
-            ax.axis[direction].set_visible(False)
-
-        x = np.linspace(-0.5, 1., 100)
-        ax.plot(x, np.sin(x*np.pi))
-
-        plt.show()
-        quit()
     app = QApplication()
     window = MainWindow() 
     window.show()
